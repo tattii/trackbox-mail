b8a5a92d57796230beeb68cea5f8f307d8a5824f
tattii <yuta.tatti@gmail.com>
2014-12-25 05:35:47 +0900

    return mail

--- index.js
+++ index.js
@@ -9,28 +9,19 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	response.send('Hello World!');
 });
 
 
 app.get('/post', function(req, res) {
-  var data = req.param();
-  
-  console.log("post");
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			client.query('INSERT INTO test (id, data) VALUES ($1, $2)', ["test0001", data], function(err, result) {
-				done();
-				if (err) {
-					console.error(err);
-					res.send("Error " + err);
-				}else{
-					res.send({ id: id });
-				}
-			});
-	});
+	sendText(
+		"trackbox.link@app32823870.mailgun.org",
+		req.param("from"),
+		"test"
+	);
 
-  res.send("");
+	res.send("");
 });
 
 
@@ -40,5 +31,5 @@ mg.createRoute(".*?", "http://trackbox-mail.herokuapp.com/post");
 
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+	console.log("Node app is running at localhost:" + app.get('port'));
 });
